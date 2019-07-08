25a8c44dc287bc8efa6ca9cd35d7429b0767a480
Airstryk-1 <cj2027@yahoo.com>
2019-07-08 12:28:11 -0700

    server changes

--- gaming-analytics-server/src/Data/data-router.js
+++ gaming-analytics-server/src/Data/data-router.js
@@ -7,19 +7,10 @@ const route = express.Router();
 const fetch = require('node-fetch');
 fetch.Promise = global.Promise;
 
-const url = "https://api.twitch.tv/helix/analytics/games";
-const key = process.env.CLIENT_ID;
-const secret = process.env.CLIENT_SECRET;
-const token = process.env.ACCESS_TOKEN;
-console.log(key, secret)
+const url = "https://rapidapi.com/stefan.skliarov/api/SteamWeb";
+const key = process.env.API_KEY;
 const options = {
-  method: 'GET',
-  headers: {
-    'Client-ID': key,
-    'Accept': 'application/vnd.twitchtv.v5+json',
-    'Authorization': 'Bearer' + token,
-  },
-  'Authentication': { 'scope': ['analytics:read:games'] }
+  method: 'GET'
 }
 
 route.get('/api', (req,res) => {
--- gaming-analytics-server/src/server.js
+++ gaming-analytics-server/src/server.js
@@ -2,9 +2,58 @@ const express = require('express');
 const app = require('../src/app');
 const route = require('../src/Data/data-router');
 const { PORT } = require('./config')
+const fetch = require('node-fetch');
+const knex = require('knex');
+const apiKey = 'CE6952659902BFF7DE3C71F8203059A1';
+const DB = 'postgres://fztxooam:QqYFC14XdIvOvdp8rkpPhX4bz4E2yglm@raja.db.elephantsql.com:5432/fztxooam';
+const getIDs = `https://api.steampowered.com/ISteamApps/GetAppList/v1/?key=${apiKey}`;
+const paramsLimit = 31906;
+const limit = paramsLimit / 2;
 
 app.use(express.static('public'));
 app.use('/', route);
+
+fetch(getIDs)
+  .then(res => res.json())
+  .then(res => {
+
+    const chunk1 = [], chunk2 = [], chunk3 = [], chunk4 = [], chunk5 = [], chunk6 = [];
+
+    res.applist.apps.app.forEach((val, index) => {
+      if (index >= 0 && index < limit) {
+        chunk1.push(val)
+      } else if (index >= limit && index < (limit * 2)) {
+        chunk2.push(val)
+      } else if (index >= (limit * 2) && index < (limit * 3)) {
+        chunk3.push(val)
+      } else if (index >= (limit * 3) && index < (limit * 4)) {
+        chunk4.push(val)
+      } else if (index >= (limit * 4) && index < (limit * 5)) {
+        chunk5.push(val)
+      } else if (index >= (limit * 5) && index < (limit * 6)) {
+        chunk6.push(val)
+      }
+    });
+
+    const chunks = [chunk1, chunk2, chunk3, chunk4, chunk5, chunk6];
+
+    db.transaction(trx =>
+      Promise.all([
+        chunks.forEach(chunk =>
+          db('gamesids')
+            .transacting(trx)
+            .insert(chunk)
+            .catch(err => {
+              console.log('Went Wrong', err)
+            })
+        )
+      ])
+    )
+
+  })
+  .catch(err => {
+    console.log('Error', err)
+  })
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`)
